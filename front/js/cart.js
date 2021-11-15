26e2f8e179603ac17462414a5ed4441d3fd80018
Denizot <denizot.myriam@gmail.com>
2021-11-15 15:10:33 +0100

    teste local storage

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,1 +1,60 @@
 
+
+
+  // Element HTML du cart
+  const cartSection = document.getElementById("cart__items");
+  const cartOrder = document.getElementsByClassName("cart__order");
+  const cartPrice = document.getElementsByClassName("cart__price");
+  const h1 = document.getElementsByTagName("h1");
+  
+  // fetch 
+  function fetchIdData() {
+    let selectedProducts = getCart();
+    let quantity = 0;
+    let price = 0;
+    if (localStorage.getItem("panier") != null) {
+      for (let i = 0; i < selectedProducts.length; i++) {
+        let id = selectedProducts[i][0];
+        let color = selectedProducts[i][1];
+        let url = "http://localhost:3000/api/products/" + id;
+        fetch(url)
+          .then((response) => response.json())
+          .then((data) => {
+            cartSection.innerHTML += `<article class="cart__item" data-id="${id}" data-color="${color}">
+                  <div class="cart__item__img">
+                    <img src="${data.imageUrl}" alt="${data.altTxt}">
+                  </div>
+                  <div class="cart__item__content">
+                    <div class="cart__item__content__titlePrice">
+                      <h2>${data.name}</h2>
+                      <p>${data.price} €</p>
+                    </div>
+                    <div class="cart__item__content__settings">
+                      <div class="cart__item__content__settings__quantity">
+                        <p>Qté : </p>
+                        <input type="number" class="itemQuantity" name="itemQuantity" onchange="changeQuantity('${id}', '${color}', this.value)" min="1" max="100" value="${selectedProducts[i][2]}">
+                      </div>
+                      <div class="cart__item__content__settings__delete">
+                        <p class="deleteItem" onclick="deleteItem()">Supprimer</p>
+                      </div>
+                    </div>
+                  </div>
+                </article>`;
+            // total price (if quantity (selectedProducts[i][2]))
+            price += data.price * selectedProducts[i][2];
+            document.getElementById("totalPrice").innerHTML = price;
+          });
+  
+        // total Quantity
+        quantity += selectedProducts[i][2];
+        document.getElementById("totalQuantity").innerHTML = quantity;
+      }
+    } else {
+      h1[0].innerHTML = `Votre panier est vide`;
+      cartOrder[0].innerHTML = "";
+      cartPrice[0].innerHTML = "";
+    }
+  }
+
+  fetchIdData();
+
--- front/js/product.js
+++ front/js/product.js
@@ -6,7 +6,7 @@ const objectURL= "http://localhost:3000/api/products/" + id;
 console.log("url fetch récupérer est " + objectURL);
 
 // Création de la fonction qui recupere via le fetch les données dans le Json et les modifies dans le DOM
-let ProductCard = function () {
+let printProductCard = function () {
     fetch(objectURL)
       .then((response) => response.json())
       .then((data) => {
@@ -31,36 +31,80 @@ let ProductCard = function () {
         for (i = 0; i < data.colors.length; i++) {
           color.innerHTML += `<option value="${data.colors[i]}">${data.colors[i]}</option>`;
         }
-      });
+      })
+
+      .catch((err) => { //si requete impossible affichage message d'erreur  et message erreur console
+        document.getElementsByClassName('item').innerText = 'Désolé un problème est survenu pendant le chargement du produit.veuillez réessayer ultérieurement'
+        console.log('Problème API Page Produit');
+          });
   };
 
-// .catch((err) => { //si requete impossible affichage message d'erreur  et message erreur console
-//     document.getElementById('item').innertext = 'Désolé un problème est survenu pendant le chargement de notre catalogue.veuillez réessayer ultérieurement'
-//     console.log('Problème API');
-//       });
+
 
   // Appelle de la fonction crée.
-  ProductCard();
+  printProductCard();
 
 
   // La fonction qui recupere la valeur de la couleur 
-function ColorChoice() {
-  let colorK = document.getElementById("colors");
-  return colorK.value;
+function getColor() {
+  let color = document.getElementById("colors");
+  return color.value;
 }
   // La fonction qui recupere la valeur du champs quantity 
-function quantityProduct() {
-  let productQuantity = document.getElementById("quantity");
-  return productQuantity.value;
+function getQuantity() {
+  let quantity = document.getElementById("quantity");
+  return quantity.value;
 }
 
+
 const sendBtnProductCart = document.getElementById("addToCart");
-// Evenement sendBtnProductCart, fonction ProductCard qui active les autres fonction au click
+// Evenement sendBtnProductCart, fonction printProductCard qui active les autres fonction au click
 sendBtnProductCart.addEventListener("click", () => {
-  let productQuantity = quantityProduct();
-  let colorK = ColorChoice();
-  //addProductToCart(id, productQuantity ,colorK);
-  console.log(`la couleur est ${colorK} et la quantité est ${productQuantity}`);
+  let quantity = getQuantity();
+  let color = getColor();
+  addProductToCart(id, quantity ,color);
+  console.log(`la couleur est ${color} et la quantité est de ${quantity}`);
 });
 
 //addProductToCart --> Crée une fonction dans le panier pour ajouter les valeurs récuperer ici .
+
+
+
+// fonction getCart qui permet de mettre dans le local storage
+function getCart() {
+  let selectedProducts = []; // variable selectedProducts avec un tableau vide à remplir
+  if (localStorage.getItem("panier") != null) {
+    selectedProducts = JSON.parse(localStorage.getItem("panier"));
+  }
+  return selectedProducts;
+}
+
+
+function addProductToCart(id, quantity, color) {
+  if (quantity == 0) {
+    return;
+  }
+  let selectedProducts = getCart();
+  if (selectedProducts.length == 0) {
+    selectedProducts = [[id, quantity, color]];
+  } else {
+    let found = false;
+    for (let i = 0; i < selectedProducts.length; i++) {
+      if (id === selectedProducts[i][0] && color === selectedProducts[i][1]) {
+        found = true;
+        selectedProducts[i][2] += quantity;
+      }
+    }
+    if (found == false) {
+      let selectedProduct = [id, quantity, color];
+      selectedProducts.push(selectedProduct);
+    }
+  }
+  localStorage.setItem("panier", JSON.stringify(selectedProducts));
+
+  console.log(selectedProducts); //log recupere tableau 
+}
+
+
+
+
